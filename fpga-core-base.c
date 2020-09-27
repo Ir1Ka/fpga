bf3e3c51f7f728813923c3ff69265c0430e97ccc
IriKa <qiujie.jq@gmail.com>
2020-09-27 11:09:35 +0800

    Fix compile warning for k2.6.32.y
    * fpga-core-base.c:309:7: warning: passing argument 1 of 'resource_size'
    			  discards 'const' qualifier from pointer target type
    			  [enabled by default]
    
    Signed-off-by: IriKa <qiujie.jq@gmail.com>

--- fpga-core-base.c
+++ fpga-core-base.c
@@ -306,7 +306,7 @@ __fpga_new_ip(struct fpga *fpga, struct fpga_ip_info const *info)
 		}
 
 		if ((info->resources[i].start & aligned) != aligned ||
-		    (resource_size(&info->resources[i]) & aligned) != aligned) {
+		    (resource_size((struct resource *)&info->resources[i]) & aligned) != aligned) {
 			dev_err(&fpga->dev, "resources are not aligned\n");
 			return ERR_PTR(-EINVAL);
 		}
