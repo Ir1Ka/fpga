9d417d773d4d22b5aeae92f8cfa986b1cc13bf3a
IriKa <qiujie.jq@gmail.com>
2020-09-27 12:07:34 +0800

    Add resource attribute for FPGA and IP
    Signed-off-by: IriKa <qiujie.jq@gmail.com>

--- fpga-core-base.c
+++ fpga-core-base.c
@@ -168,6 +168,39 @@ name_show(struct device *dev, struct device_attribute *attr, char *buf)
 }
 static DEVICE_ATTR_RO(name);
 
+static ssize_t resource_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct fpga *fpga;
+	struct fpga_ip *ip;
+	const struct resource *res = NULL;
+	unsigned int max = 0;
+	unsigned int i;
+	int idx = 0;
+
+	fpga = fpga_verify(dev);
+	ip = fpga_verify_ip(dev);
+
+	if (fpga) {
+		res = &fpga->resource;
+		max = 1;
+	} else if (ip) {
+		res = ip->resources;
+		max = ip->num_resources;
+	}
+
+	if (!res || !max)
+		return -ENODEV;
+
+	for (i = 0; i < max; i++)
+		idx += snprintf(buf + idx, PAGE_SIZE - idx, "0x%016llx 0x%016llx 0x%016llx\n",
+				(unsigned long long)res[i].start,
+				(unsigned long long)res[i].end,
+				(unsigned long long)res[i].flags);
+
+	return idx;
+}
+static DEVICE_ATTR(resource, S_IRUGO, resource_show, NULL);
+
 static ssize_t
 modalias_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
@@ -228,6 +261,7 @@ static struct attribute *fpga_ip_attrs[] = {
 	&dev_attr_name.attr,
 	&dev_attr_modalias.attr,
 	&dev_attr_remove.attr,
+	&dev_attr_resource.attr,
 	NULL,
 };
 ATTRIBUTE_GROUPS(fpga_ip);
@@ -555,6 +589,7 @@ static struct attribute *fpga_attrs[] = {
 	&dev_attr_name.attr,
 	&dev_attr_new_ip.attr,
 	&dev_attr_delete_ip.attr,
+	&dev_attr_resource.attr,
 	NULL,
 };
 ATTRIBUTE_GROUPS(fpga);
