00119699e98dc8b4f51d2290655b2f18e28beb30
IriKa <qiujie.jq@gmail.com>
2020-09-27 16:21:08 +0800

    Optimize fpga-dev
    * cmd is composed of type, op, idx, and size.
    * Use the high-level package access interface in fpga-core.
    
    Signed-off-by: IriKa <qiujie.jq@gmail.com>

--- fpga-core.h
+++ fpga-core.h
@@ -4,6 +4,8 @@
 #if defined(__KERNEL) || defined(__KERNEL__)
 #include <linux/init.h>
 
+#include <fpga.h>
+
 struct fpga;
 
 #if IS_ENABLED(CONFIG_OF)
@@ -15,6 +17,11 @@ void of_fpga_register_ips(struct fpga *fpga);
 static inline void of_fpga_register_ips(struct fpga *fpga) { }
 
 #endif /* CONFIG_OF */
+
+int fpga_reg_read(const struct fpga_ip *ip, int size, int index, u64 where,
+		  union fpga_reg_data *reg);
+int fpga_reg_write(const struct fpga_ip *ip, int size, int index, u64 where,
+		   union fpga_reg_data reg);
 
 int __init fpga_dev_init(void);
 void __exit fpga_dev_exit(void);
