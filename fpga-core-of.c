4a07830b2ec9caaf168139c85f1071aabf2074b1
IriKa <qiujie.jq@gmail.com>
2020-10-16 10:52:55 +0800

    (Patch 2) Add bypass reg and block access IF
    The remaining part in patch "Add bypass register and block access interface".
    
    Signed-off-by: IriKa <qiujie.jq@gmail.com>

--- fpga-core-of.c
+++ fpga-core-of.c
@@ -145,7 +145,7 @@ int of_fpga_get_ip_info(struct device *dev, struct device_node *node,
 		dev_warn(dev, "Using DTS on the PowerPC architecture to "
 			      "describe FPGA may not work perperly!\n");
 
-	while (!of_address_to_resource(node, cnt, &info->resources[cnt])) {
+	while (!of_address_to_resource(node, cnt, &info->resources[cnt].resource)) {
 		if (cnt > FPGA_NUM_RESOURCES_MAX) {
 			dev_err(dev, "of_fpga: Max support %d resources\n",
 				FPGA_NUM_RESOURCES_MAX);
