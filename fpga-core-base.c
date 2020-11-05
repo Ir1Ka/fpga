d28ce77e2cd0468774e5826f33d032d811443fc1
IriKa <qiujie.jq@gmail.com>
2020-11-05 11:42:26 +0800

    Optimize `bits_attr_store`
    If this attribute occupies all bits of the register, then there is no need to
    read the register first to reserve other bits, because there are no other bits.
    
    Signed-off-by: IriKa <qiujie.jq@gmail.com>

--- fpga-core-base.c
+++ fpga-core-base.c
@@ -1470,9 +1470,16 @@ ssize_t bits_attr_store(struct device *dev, struct device_attribute *attr,
 		     off + bits > size * 8))
 		return -EIO;
 
-	res = fpga_reg_xfer(fpga, where, FPGA_READ, size, &reg);
-	if (unlikely(res))
-		return res;
+	/*
+	 * If this attribute occupies all bits of the register,
+	 * then there is no need to read the register first to reserve other bits,
+	 * because there are no other bits.
+	 */
+	if (bits != size * 4) {
+		res = fpga_reg_xfer(fpga, where, FPGA_READ, size, &reg);
+		if (unlikely(res))
+			return res;
+	}
 
 	res = strict_strtoull(buf, 0, &t);
 	if (res)
